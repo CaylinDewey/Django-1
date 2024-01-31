8efeb74e25842d011114c55549cfe864e8e3e5b6
Caylin Dewey <caylindewey42@gmail.com>
2024-01-31 09:36:45 +0000

    Modified requirements, settings and Procfile for deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Django-1.wsgi
--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-x8um@_ctj9ubz9#(sz1vml_awykhn#=#f#qvo-j!#=v5_ml4^)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8000-caylindewey-django1-yl5g0egpyhh.ws-eu107.gitpod.io"]
+ALLOWED_HOSTS = ["8000-caylindewey-django1-yl5g0egpyhh.ws-eu107.gitpod.io,'.herokuapp.com'"]
 
 
 # Application definition
